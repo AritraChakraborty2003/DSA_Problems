a230daabf1cf936aeeaa8524fe29025d878f3524
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-12-20 23:29:33 +0530

    Update: Added the Search in LL and Array2LL code in java

--- LinkedList/EasyProblems/ArrayToLL.java
+++ LinkedList/EasyProblems/ArrayToLL.java
@@ -1,8 +1,50 @@
 package LinkedList.EasyProblems;
 
-
+import java.util.ArrayList;
 
 public class ArrayToLL {
     
+    static Node Array2LL(ArrayList<Integer> arr){
 
+        Node temp = new Node(arr.get(0));
+        Node mover=temp;
+        for(int i=1;i<arr.size();i++){
+            mover.next = new Node(arr.get(i));
+            mover = mover.next;
+        }
+        return temp;
+    }
+
+    static void printLL(Node head){
+        Node temp = head;
+        while(temp!= null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(5);
+        arr.add(7);
+        arr.add(9);
+        arr.add(3);
+
+        Node head=Array2LL(arr);
+        printLL(head);
+    }
+}
+
+
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
 }
--- LinkedList/EasyProblems/SearchInLL.java
+++ LinkedList/EasyProblems/SearchInLL.java
@@ -1,5 +1,48 @@
 package LinkedList.EasyProblems;
 
 public class SearchInLL {
-    
+
+    static void printLL(Node head){
+        Node temp = head;
+        while(temp!= null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    static boolean search(Node head, int key){
+        Node temp = head;
+        while(temp!= null){
+            if(temp.data == key)
+                return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Node head=new Node(6);
+        head.next = new Node(8);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(7);
+
+        int key = 1;
+        int key1= 3;
+
+        System.out.print("Original Linked List: ");
+        printLL(head);
+
+        System.out.println("Element "+key+" found in LinkedList: "+search(head, key));
+        System.out.println("Element "+key1+" found in LinkedList: "+search(head, key1));
+    }
+}
+
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
 }
