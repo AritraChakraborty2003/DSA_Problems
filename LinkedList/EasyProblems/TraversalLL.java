554938fef57402e2380ccf8badd6d5799463e1d7
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-12-20 22:51:19 +0530

    Setup:Initial setup with LL problems

---
+++ LinkedList/EasyProblems/TraversalLL.java
@@ -0,0 +1,36 @@
+package LinkedList.EasyProblems;
+
+public class TraversalLL {
+    static void printLL(Node head){
+        Node temp = head;
+        while(temp!= null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        System.out.println("Original Linked List:");
+        printLL(head);
+
+    }
+    
+}
+
+
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
